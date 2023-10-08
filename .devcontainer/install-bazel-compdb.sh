c78be1e577f0ec0c7ef497df5e6bbd73516bef66
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-10-08 00:15:46 -0700

    Improve install-bazel-compdb

--- .devcontainer/install-bazel-compdb.sh
+++ .devcontainer/install-bazel-compdb.sh
@@ -10,8 +10,21 @@ if [ "${BAZEL_COMPDB_VERSION}" = "none" ]; then
     exit 0
 fi
 
-cd /opt/bazel
+# Cleanup temporary directory and associated files when exiting the script.
+cleanup() {
+    EXIT_CODE=$?
+    set +e
+    if [[ -n "${TMP_DIR}" ]]; then
+        echo "Executing cleanup of tmp files"
+        rm -Rf "${TMP_DIR}"
+    fi
+    exit $EXIT_CODE
+}
+trap cleanup EXIT
 
-curl -L "https://github.com/grailbio/bazel-compilation-database/archive/${BAZEL_COMPDB_VERSION}.tar.gz" | tar -xz \
+echo "Installing bazel-compdb..."
+TMP_DIR=$(mktemp -d -t bazel-compdb-XXXXXXXXXX)
 
+cd ${TMP_DIR}
+curl -L "https://github.com/grailbio/bazel-compilation-database/archive/${BAZEL_COMPDB_VERSION}.tar.gz" | tar -xz
 ln -f -s "/opt/bazel/bazel-compilation-database-${BAZEL_COMPDB_VERSION}/generate.py" "/usr/local/bin/bazel-compdb"
