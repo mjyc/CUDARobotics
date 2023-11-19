28a06828f1bd60973fd2d3d852927fcbecf4f32a
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-11-19 02:46:40 -0800

    Update arm navigation

--- .scripts/Planning/arm_navigation/arm_navigation.py
+++ .scripts/Planning/arm_navigation/arm_navigation.py
@@ -5,28 +5,37 @@ from json import load
 
 import matplotlib.pyplot as plt
 
-# def save_astar_solution_as_png(
-#     obstacles: list[Cell],
-#     start: Cell,
-#     goal: Cell,
-#     path: list[Cell],
-#     visited: list[Cell],
-#     filename: str = "astar.png",
-# ) -> None:
-#     ox, oy = [list(lst) for lst in zip(*obstacles)] if obstacles else [[], []]
-#     sx, sy = start
-#     gx, gy = goal
-#     px, py = [list(lst) for lst in zip(*path)] if path else [[], []]
-#     vx, vy = [list(lst) for lst in zip(*visited)] if visited else [[], []]
-#     plt.plot(ox, oy, ".k")
-#     plt.plot(vx, vy, "xc")
-#     plt.plot(sx, sy, "og")
-#     plt.plot(gx, gy, "xb")
-#     plt.plot(px, py, "-r")
-#     plt.grid(True)
-#     plt.axis("equal")
-#     plt.savefig(filename)
-#     plt.close()
+from math import cos, sin
+
+NUM_LINK = 3
+
+
+def save_ik_path_as_png(
+    joint_angles: list[float, float, float],
+    link_lengths: list[float, float, float],
+    target: list[float, float],
+    path: list[list[float, float, float]],
+) -> None:
+    for i, j_angles in enumerate([joint_angles] + path):
+        pts = [[0, 0] for _ in range(NUM_LINK + 1)]
+        for i in range(1, NUM_LINK + 1):
+            pts[i][0] = pts[i - 1][0] + link_lengths[i - 1] * cos(sum(j_angles[:i]))
+            pts[i][1] = pts[i - 1][1] + link_lengths[i - 1] * sin(sum(j_angles[:i]))
+        for i in range(NUM_LINK + 1):
+            if i != NUM_LINK:
+                plt.plot(
+                    [pts[i][0], pts[i + 1][0]],
+                    [pts[i][1], pts[i + 1][1]],
+                    "r-",
+                )
+            plt.plot(pts[i][0], pts[i][1], "ko")
+
+    plt.plot(target[0], target[1], "go")
+    plt.grid(True)
+    plt.xlim([-10, 10])
+    plt.ylim([-10, 10])
+    plt.savefig(argv[2])
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -35,9 +44,8 @@ if __name__ == "__main__":
 
     with open(argv[1], "r") as file:
         data = load(file)
-        # obstacles: list[Cell] = data["obstacles"]
-        # start: Cell = data["start"]
-        # goal: Cell = data["goal"]
-        # path: list[Cell] = data["path"]
-        # visited: list[Cell] = data["visited"]
-        # save_astar_solution_as_png(obstacles, start, goal, path, visited, argv[2])
+        joint_angles: list[float, float, float] = data["joint_angles"]
+        link_lengths: list[float, float, float] = data["link_lengths"]
+        target: list[float, float] = data["target"]
+        path: list[list[float, float, float]] = data["path"]
+        save_ik_path_as_png(joint_angles, link_lengths, target, path)
--- Planning/arm_navigation/simulate.cpp
+++ Planning/arm_navigation/simulate.cpp
@@ -1,35 +1,52 @@
 #include <nlohmann/json.hpp>
 
+#include <fstream>
 #include <iostream>
-#include <mjyc.h>
+#include <vector>
+
+#include "Planning/arm_navigation/mjyc.h"
 
 using json = nlohmann::json;
 
-json ToJSON(const Eigen::Vector2f base, const Eigen::Vector3f joint_angles,
-            const Eigen::Vector3f link_lengths, const Eigen::Vector2f target)
+json ToJSON(const Eigen::Vector3f& joint_angles,
+            const Eigen::Vector3f& link_lengths, const Eigen::Vector2f& target,
+            const std::vector<Eigen::Vector3f>& path)
 {
   json j;
-  j["base"] = base;
   j["joint_angles"] = joint_angles;
   j["link_lengths"] = link_lengths;
   j["target"] = target;
+  j["path"] = json::array();
+  for (const Eigen::Vector3f& joint_angles : path)
+    j["path"].push_back(joint_angles);
   return j;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "Hello, World!" << std::endl;
+  Eigen::Vector3f joint_angles{1.57, 0.0, 0.0};
+  Eigen::Vector3f link_lengths{3.0, 2.0, 1.0};
+  Eigen::Vector2f target{2.0, 2.0};
 
-  Eigen::Vector2f base{1.0, 2.0};
-  Eigen::Vector3f joint_angles{0.0, 0.0, 0.0};
-  Eigen::Vector3f links{0.0, 0.0, 0.0};
-  Eigen::Vector2f target{0.0, 0.0};
+  // Run IK
+  std::vector<Eigen::Vector3f> path;
+  InverseKinematics(link_lengths, joint_angles, target, path);
 
-  std::cout << ToJSON(base, joint_angles, links, target).dump() << std::endl;
-  // ThreeLinkArm robot{Eigen::Vector2f{1.0, 2.0}};
-
-  // std::cout << robot.JointAngles().transpose() << std::endl;
-  // std::cout << robot.JointAngles().transpose() << std::endl;
+  // Print or save everything
+  std::ofstream file;
+  std::ostream* out = &std::cout;
+  if (argc > 1)
+  {
+    std::string filename = argv[1];  // NOLINT
+    file.open(filename);
+    if (!file)
+    {
+      std::cerr << "Error opening file: " << filename << std::endl;
+      return 1;
+    }
+    out = &file;
+  }
+  *out << ToJSON(joint_angles, link_lengths, target, path).dump() << std::endl;
 
   return 0;
 }
